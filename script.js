bacffe26f900c1540f062f8a3258d82cbd0d0796
infi <91536265+Infii69@users.noreply.github.com>
2024-12-14 17:06:34 +0530

    ssssssssssssss

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Smooth scrolling to sections
-document.querySelectorAll('nav ul li a').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
+
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
